507c6cf47d25a8dbba6424e0bc2fde92c45453aa
Hunter Parcells <hunterparcells@gmail.com>
2024-01-20 10:39:27 -0500

    feat: Generate routing number for user. (#26)

--- src/main/java/rebelalliance/smartcash/scene/RegisterController.java
+++ src/main/java/rebelalliance/smartcash/scene/RegisterController.java
@@ -4,9 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.util.Random;
 import java.util.UUID;
 
 public class RegisterController {
+    Random random = new Random();
+
     public TextField emailInput;
     public PasswordField passwordInput;
 
@@ -14,11 +17,14 @@ public class RegisterController {
     protected void onRegisterClick() {
         String email = emailInput.getText();
         String password = passwordInput.getText();
+        // These two lines should be moved to the backend.
         UUID uuid = UUID.randomUUID();
+        int routingNumber = random.nextInt(100000, 1000000);
 
         System.out.println("Email: " + email);
         System.out.println("Password: " + password);
         System.out.println("UUID: " + uuid);
+        System.out.println("Routing Number: " + routingNumber);
 
         // TODO: Call database function.
     }
