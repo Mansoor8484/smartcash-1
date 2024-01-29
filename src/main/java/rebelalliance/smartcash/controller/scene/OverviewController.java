594482a62f4eb98dc867e094cc0fec4d38ddd7dd
Hunter Parcells <hunterparcells@gmail.com>
2024-01-29 15:49:13 -0500

    feat: Properly order line chart x-axis for dates.
    Stupid graph took nearly five hours.

--- src/main/java/rebelalliance/smartcash/controller/scene/OverviewController.java
+++ src/main/java/rebelalliance/smartcash/controller/scene/OverviewController.java
@@ -2,9 +2,7 @@ package rebelalliance.smartcash.controller.scene;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.PieChart;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 import javafx.scene.layout.VBox;
 import rebelalliance.smartcash.account.Account;
 import rebelalliance.smartcash.component.BigNumber;
@@ -12,12 +10,10 @@ import rebelalliance.smartcash.controller.BaseController;
 import rebelalliance.smartcash.controller.IController;
 import rebelalliance.smartcash.scene.SCScene;
 import rebelalliance.smartcash.statistic.LedgerStats;
-import rebelalliance.smartcash.util.ArrayUtil;
 import rebelalliance.smartcash.util.DateUtil;
 
-import java.lang.reflect.Array;
 import java.time.LocalDate;
-import java.util.Arrays;
+import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.List;
 import java.util.SortedMap;
@@ -28,13 +24,26 @@ public class OverviewController extends BaseController implements IController {
     @FXML
     private PieChart pieChart;
     @FXML
-    private LineChart<String, Double> accountsWeekOverWeek;
+    private LineChart<Number, Number> accountsWeekOverWeek;
+    @FXML
+    private NumberAxis lineChartXAxis;
 
     private LedgerStats ledgerStats;
 
     @Override
     public void init() {
         this.ledgerStats = new LedgerStats(this.sceneManager.getLedger());
+
+        this.lineChartXAxis.setForceZeroInRange(false);
+        this.lineChartXAxis.setTickLabelFormatter(new NumberAxis.DefaultFormatter(this.lineChartXAxis) {
+            @Override
+            public String toString(Number object) {
+                return DateUtil.format(LocalDate.ofEpochDay(object.longValue() / (60 * 60 * 24)));
+            }
+        });
+        this.lineChartXAxis.setMinorTickVisible(false);
+        this.lineChartXAxis.setTickUnit(60 * 60 * 24);
+        this.lineChartXAxis.setAutoRanging(false);
 
         this.update();
     }
@@ -63,26 +72,24 @@ public class OverviewController extends BaseController implements IController {
         this.pieChart.setLabelsVisible(true);
 
         // Line graph.
-        HashMap<Account, XYChart.Series<String, Double>> data = new HashMap<>();
+        HashMap<Account, XYChart.Series<Number, Number>> data = new HashMap<>();
         for(Account account : accounts) {
-            XYChart.Series<String, Double> series = new XYChart.Series<>();
+            XYChart.Series<Number, Number> series = new XYChart.Series<>();
             series.setName(account.toString());
-
             data.put(account, series);
         }
-
         SortedMap<LocalDate, HashMap<Account, Double>> dayOverDay = this.ledgerStats.getBalanceDayOverDay();
-
         for(LocalDate date : dayOverDay.keySet()) {
             HashMap<Account, Double> dayBalances = dayOverDay.get(date);
             for(Account account : dayBalances.keySet()) {
-                XYChart.Series<String, Double> series = data.get(account);
-                series.getData().add(new XYChart.Data<>(DateUtil.format(date), dayBalances.get(account)));
+                XYChart.Series<Number, Number> series = data.get(account);
+                series.getData().add(new XYChart.Data<>(date.atStartOfDay(ZoneId.systemDefault()).toEpochSecond(), dayBalances.get(account)));
             }
         }
-
-        this.accountsWeekOverWeek.getData().clear();
         this.accountsWeekOverWeek.getData().setAll(data.values());
+
+        this.lineChartXAxis.setLowerBound(this.sceneManager.getLedger().getLedger().get(0).getDate().minusDays(1).atStartOfDay(ZoneId.systemDefault()).toEpochSecond());
+        this.lineChartXAxis.setUpperBound(this.sceneManager.getLedger().getLedger().get(this.sceneManager.getLedger().getLedger().size() - 1).getDate().plusDays(1).atStartOfDay(ZoneId.systemDefault()).toEpochSecond());
     }
 
     public void testGoToTransactions(ActionEvent actionEvent) {
