cdfcc9cde7b02b3d3491646e7912aee42b9ab1f5
Hunter Parcells <hunterparcells@gmail.com>
2024-02-02 15:18:13 -0500

    feat: Show/hide composition graph accounts.

--- src/main/java/rebelalliance/smartcash/controller/scene/OverviewController.java
+++ src/main/java/rebelalliance/smartcash/controller/scene/OverviewController.java
@@ -2,7 +2,10 @@ package rebelalliance.smartcash.controller.scene;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
 import javafx.scene.chart.*;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import rebelalliance.smartcash.ledger.container.Account;
 import rebelalliance.smartcash.component.BigNumber;
@@ -23,38 +26,47 @@ public class OverviewController extends BaseController implements IController {
     @FXML
     private VBox accountsBox;
     @FXML
-    private PieChart pieChart;
+    private PieChart compositionPieChart;
     @FXML
-    private LineChart<Number, Number> accountsWeekOverWeek;
+    private LineChart<Number, Number> historicalLineChart;
     @FXML
-    private NumberAxis lineChartXAxis;
+    private NumberAxis historicalLineChartXAxis;
     @FXML
     private PieChart spendPieChart;
 
     private LedgerStats ledgerStats;
 
+    private HashMap<Account, Boolean> accountCompositionDisplay;
+
     @Override
     public void init() {
         this.ledgerStats = new LedgerStats(this.sceneManager.getLedger());
+        this.accountCompositionDisplay = new HashMap<>();
 
-        this.lineChartXAxis.setForceZeroInRange(false);
-        this.lineChartXAxis.setTickLabelFormatter(new NumberAxis.DefaultFormatter(this.lineChartXAxis) {
+        this.historicalLineChartXAxis.setForceZeroInRange(false);
+        this.historicalLineChartXAxis.setTickLabelFormatter(new NumberAxis.DefaultFormatter(this.historicalLineChartXAxis) {
             @Override
             public String toString(Number object) {
                 return DateUtil.format(LocalDate.ofEpochDay(object.longValue() / (60 * 60 * 24)));
             }
         });
-        this.lineChartXAxis.setMinorTickVisible(false);
-        this.lineChartXAxis.setTickUnit(60 * 60 * 24);
-        this.lineChartXAxis.setAutoRanging(false);
+        this.historicalLineChartXAxis.setMinorTickVisible(false);
+        this.historicalLineChartXAxis.setTickUnit(60 * 60 * 24);
+        this.historicalLineChartXAxis.setAutoRanging(false);
 
         this.update();
     }
 
     @Override
     public void update() {
-        this.accountsBox.getChildren().clear();
+        this.updateAccountDisplay();
+        this.updateCompositionPieChart();
+        this.updateLineGraph();
+        this.updateSpendPieChart();
+    }
 
+    public void updateAccountDisplay() {
+        this.accountsBox.getChildren().clear();
         List<Account> accounts = this.sceneManager.getLedger().getAccounts();
         for(Account account : accounts) {
             if(account.isArchived()) {
@@ -65,10 +77,23 @@ public class OverviewController extends BaseController implements IController {
             bigNumber.setAmount(account.getBalance());
             this.accountsBox.getChildren().add(bigNumber);
         }
+    }
 
-        this.pieChart.getData().clear();
-        for(Account account : accounts) {
-            if(account.isArchived()) {
+    public void updateCompositionPieChart() {
+        this.compositionPieChart.getData().clear();
+        ContextMenu compositionPieChartContextMenu = new ContextMenu();
+        for(Account account : this.sceneManager.getLedger().getAccounts()) {
+            if(!this.accountCompositionDisplay.containsKey(account)) {
+                this.accountCompositionDisplay.put(account, true);
+            }
+            MenuItem menuItem = new MenuItem((this.accountCompositionDisplay.get(account) ? "Hide " : "Show ") + account);
+            menuItem.setOnAction(event -> {
+                this.accountCompositionDisplay.put(account, !this.accountCompositionDisplay.get(account));
+                this.updateCompositionPieChart();
+            });
+            compositionPieChartContextMenu.getItems().add(menuItem);
+
+            if(account.isArchived() || !this.accountCompositionDisplay.get(account)) {
                 continue;
             }
             double balance = account.getBalance();
@@ -76,14 +101,17 @@ public class OverviewController extends BaseController implements IController {
                 continue;
             }
 
-            this.pieChart.getData().add(new PieChart.Data(account.toString(), balance));
+            this.compositionPieChart.getData().add(new PieChart.Data(account.toString(), balance));
         }
-        this.pieChart.setLabelsVisible(true);
+        this.compositionPieChart.setOnContextMenuRequested(event -> compositionPieChartContextMenu.show(this.compositionPieChart, event.getScreenX(), event.getScreenY()));
+        this.compositionPieChart.setCursor(Cursor.HAND);
+    }
 
-        // Line graph.
-        this.accountsWeekOverWeek.getData().clear();
+    public void updateLineGraph() {
+        this.historicalLineChart.getData().clear();
+
         HashMap<Account, XYChart.Series<Number, Number>> data = new HashMap<>();
-        for(Account account : accounts) {
+        for(Account account : this.sceneManager.getLedger().getAccounts()) {
             if(account.isArchived()) {
                 continue;
             }
@@ -91,6 +119,7 @@ public class OverviewController extends BaseController implements IController {
             series.setName(account.toString());
             data.put(account, series);
         }
+
         SortedMap<LocalDate, HashMap<Account, Double>> dayOverDay = this.ledgerStats.getBalanceDayOverDay();
         for(LocalDate date : dayOverDay.keySet()) {
             HashMap<Account, Double> dayBalances = dayOverDay.get(date);
@@ -102,13 +131,16 @@ public class OverviewController extends BaseController implements IController {
                 series.getData().add(new XYChart.Data<>(date.atStartOfDay(ZoneId.systemDefault()).toEpochSecond(), dayBalances.get(account)));
             }
         }
-        this.accountsWeekOverWeek.getData().setAll(data.values());
+        this.historicalLineChart.getData().setAll(data.values());
 
-        this.lineChartXAxis.setLowerBound(this.sceneManager.getLedger().getLedger().get(0).getDate().minusDays(1).atStartOfDay(ZoneId.systemDefault()).toEpochSecond());
-        this.lineChartXAxis.setUpperBound(this.sceneManager.getLedger().getLedger().get(this.sceneManager.getLedger().getLedger().size() - 1).getDate().plusDays(1).atStartOfDay(ZoneId.systemDefault()).toEpochSecond());
+        this.historicalLineChartXAxis.setLowerBound(this.sceneManager.getLedger().getLedger().get(0).getDate().minusDays(1).atStartOfDay(ZoneId.systemDefault()).toEpochSecond());
+        this.historicalLineChartXAxis.setUpperBound(this.sceneManager.getLedger().getLedger().get(this.sceneManager.getLedger().getLedger().size() - 1).getDate().plusDays(1).atStartOfDay(ZoneId.systemDefault()).toEpochSecond());
 
-        // Category Spend
+    }
+
+    public void updateSpendPieChart() {
         this.spendPieChart.getData().clear();
+
         HashMap<Category, Double> categorySpend = this.ledgerStats.getCategorySpend(LocalDate.now().minusDays(7), LocalDate.now());
         for(Category category : categorySpend.keySet()) {
             if(category.isArchived()) {
