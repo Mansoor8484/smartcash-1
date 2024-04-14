c4959bbe0e11095e320c835ac5cb05577885c893
BranMerx <156483713+BranMerx@users.noreply.github.com>
2024-04-13 23:08:41 -0400

    Account Table and UserDetails table usable

--- src/main/java/rebelalliance/smartcash/database/DatabaseConnections/src/AzureDatabaseConnection.java
+++ src/main/java/rebelalliance/smartcash/database/DatabaseConnections/src/AzureDatabaseConnection.java
@@ -4,7 +4,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Random;
-import java.util.Date;
+import rebelalliance.smartcash.database.User;
+import rebelalliance.smartcash.ledger.container.Account;
 
 public class AzureDatabaseConnection{
      public static void main(String[] args){
@@ -25,71 +26,62 @@ public class AzureDatabaseConnection{
             e.printStackTrace();
         }
     }
- 
-
-
+    //Generates the new user information and inserts it into the database
     public void DBregisterUser(String user, String pass){ //review the relationship with AccountID
         //SQL Query to insert a new user into the database
         String sql = "INSERT INTO UserAuthentication (UserID, Email, UserName, Password) VALUES (?,?,?,?)";
         //SQLQuery to insert UserID to userDetails table
+        String sql2 = "INSERT INTO UserDetails (UserID, Email) VALUES (?,?)";
         //Connection URL to the Azure SQL Database
         String url = String.format("jdbc:sqlserver://smartcash.database.windows.net:1433;database=SmartCash;user=SmartCash@smartcash;password=Winter2024@!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
 
         //Generates a new UserID
         try (Connection conn = DriverManager.getConnection(url)){
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
-            int UserID = generateUserID();
             //UserAuthentication table input
-            preparedStatement.setInt(1, UserID); // Fix: Change the argument type to String
+            int NewUserID = generateUserID();
+            preparedStatement.setInt(1, NewUserID); // Fix: Change the argument type to String
             preparedStatement.setString(2, user);
             preparedStatement.setString(3, user);
+
+            //would the hash function go here? since this is where the password would be? 
             preparedStatement.setString(4, pass);
 
+            //userDetails table input
+            //Most likely have to add the username/email as well
+            PreparedStatement preparedStatement2 = conn.prepareStatement(sql2);
+            preparedStatement2.setInt(1, NewUserID);
+            preparedStatement2.setString(2, user);
+              
             //Execute the SQL query
             preparedStatement.executeUpdate();
             System.out.println("User Registered");
+            preparedStatement2.executeUpdate();
+            System.out.println("User Details Recorded");
+            //Creates the account details in the Account table
+            createAccount(NewUserID);
+            System.out.println("Account Created");//Foreign key constraint 
         } catch(SQLException e){
             e.printStackTrace();
             //handles any kind of errors that may occur
         }
     }
-    public void inputIncome(int TransactionID, int AccountID, double Amount, Date TransactionDate, String Description){
-        //SQL Query to insert a new transaction into the database
-        String sql = "INSERT INTO Transaction (TransactionID, AccountID, Amount, Date, Description) VALUES (?,?,?,?,?)";
-        String url = String.format("jdbc:sqlserver://smartcash.database.windows.net:1433;database=SmartCash;user=SmartCash@smartcash;password=Winter2024@!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
-
-        try (Connection conn = DriverManager.getConnection(url)){
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
-            //Setting the parameters for the prepared statement
-            //Generates TransactionID
-            preparedStatement.setInt(1, TransactionID); // Fix: Change the argument type to String
-            preparedStatement.setInt(2, AccountID); // Fix: Change the argument type to String
-            preparedStatement.setDouble(3, Amount); // Fix: Change the argument type to String
-            preparedStatement.setDate(4, (java.sql.Date) TransactionDate); // Fix: Change the argument type to String
-            preparedStatement.setString(5, Description); // Fix: Change the argument type to String
-
-            //Execute the SQL query
-            preparedStatement.executeUpdate();
-            System.out.println("Transaction Recorded");
-        } catch(SQLException e){
-            e.printStackTrace();
-            //handles any kind of errors that may occur
-        }        
-    }
-
-    public void createAccount(int AccountID, int UserID, int AccountNumber, int RoutingNumber, String AccountType, double Balance){
+    //Creates the account details for the user
+    public void createAccount(int UserID){
         //SQL Query to insert a new account details into the database
-        String sql = "INSERT INTO Account (AccountID, UserID, Balance, RoutingNumber, AccountNumber) VALUES (?,?,?,?)";
+        String sql = "INSERT INTO Account(AccountID, UserID, RoutingNumber, AccountNumber) VALUES (?,?,?,?)";
         String url = String.format("jdbc:sqlserver://smartcash.database.windows.net:1433;database=SmartCash;user=SmartCash@smartcash;password=Winter2024@!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
 
         try (Connection conn = DriverManager.getConnection(url)){
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
-            //Setting the parameters for the prepared statement
-            preparedStatement.setInt(1, AccountID); // Fix: Change the argument type to String
+            //Generates AccountID
+            int NewAccountID = generateAccountID();
+            int newRoutingNumber = createRoutingNumber();
+            int newAccountNumber = createAccountNumber();
+            preparedStatement.setInt(1, NewAccountID); // Fix: Change the argument type to String
             preparedStatement.setInt(2, UserID); // Fix: Change the argument type to String
-            preparedStatement.setDouble(3, Balance); // Fix: Change the argument type to String
-            preparedStatement.setDouble(4, RoutingNumber); // Fix: Change the argument type to String
-            preparedStatement.setInt(5, AccountNumber); // Fix: Change the argument type to String  
+            preparedStatement.setInt(3, newRoutingNumber); // Fix: Change the argument type to String 
+            preparedStatement.setInt(4, newAccountNumber); // Fix: Change the argument type to String
 
             //Execute the SQL query
             preparedStatement.executeUpdate();
@@ -100,33 +92,71 @@ public class AzureDatabaseConnection{
         }
 
     }
+    public void inputTransaction(double Amount, String Description){
+        //SQL Query to insert a new transaction into the database
+        String sql = "INSERT INTO Transaction (TransactionID, Amount, Description) VALUES (?,?,?)";
+        String url = String.format("jdbc:sqlserver://smartcash.database.windows.net:1433;database=SmartCash;user=SmartCash@smartcash;password=Winter2024@!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+
+        try (Connection conn = DriverManager.getConnection(url)){
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            //Setting the parameters for the prepared statement
+            //Generates TransactionID
+            int NewTransactionID = generateTransactionID();
+            
+            preparedStatement.setInt(1, NewTransactionID); // Fix: Change the argument type to String
+            preparedStatement.setDouble(2, Amount); // Fix: Change the argument type to String
+            preparedStatement.setString(3, Description); // Fix: Change the argument type to String
+
+            //Execute the SQL query
+            preparedStatement.executeUpdate();
+            System.out.println("Transaction Recorded");
+        } catch(SQLException e){
+            e.printStackTrace();
+            //handles any kind of errors that may occur
+        }        
+    }
+
+    public void createBudget(int UserID, double BudgetAmount){
+        //SQL Query to insert a new budget into the database
+        String sql = "INSERT INTO Budget (BudgetID, AccountID, Balance) VALUES (?,?,?)";
+        String url = String.format("jdbc:sqlserver://smartcash.database.windows.net:1433;database=SmartCash;user=SmartCash@smartcash;password=Winter");
+        try (Connection conn = DriverManager.getConnection(url)){
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            //Setting the parameters for the prepared statement
+            //Generates BudgetID
+            int newBudgetID = generateBudgetID();
+            preparedStatement.setInt(1, newBudgetID); // Fix: Change the argument type to String
+            preparedStatement.setInt(2, UserID); // Fix: Change the argument type to String
+            preparedStatement.setDouble(3, BudgetAmount); // Fix: Change the argument type to String
+
+            //Execute the SQL query
+            preparedStatement.executeUpdate();
+            System.out.println("Budget Recorded");
+        } catch(SQLException e){
+            e.printStackTrace();
+            //handles any kind of errors that may occur
+        }
+    }
 
         //Function to generate a different userID for each user
         public int generateUserID(){
             //SQL Query to get the last userID from the database
             String sql = "SELECT TOP 1 UserID FROM UserAuthentication ORDER BY UserID DESC";
             String url = String.format("jdbc:sqlserver://smartcash.database.windows.net:1433;database=SmartCash;user=SmartCash@smartcash;password=Winter2024@!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
-    
+            int minUserID = 10000;
+            int maxUserID = 99999;
+            Random random = new Random();
+            int UserID = random.nextInt(maxUserID - minUserID) + minUserID;
             try (Connection conn = DriverManager.getConnection(url)){
                 PreparedStatement preparedStatement = conn.prepareStatement(sql);
                 //Execute the SQL query
                 preparedStatement.executeQuery();
-                System.out.println("UserID Generated");
             } catch(SQLException e){
                 e.printStackTrace();
                 //handles any kind of errors that may occur
             }
             //Java to actually generate the new userID
-            int UserID;
-            int minUserID = 10000;
-            int maxUserID = 99999;
-            Random random = new Random();
-            UserID = random.nextInt(maxUserID - minUserID) + minUserID;
-    
-            //Return the generated userID
-            System.out.println("Generated UserID: " + UserID);
             return UserID;
-        
         }
 
     public int generateAccountID(){
@@ -249,4 +279,6 @@ public class AzureDatabaseConnection{
         AccountNumber = random.nextInt(maxAccountNumber - minAccountNumber) + minAccountNumber;
         return AccountNumber;
     }
+    
+    
 }
